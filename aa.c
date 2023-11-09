86ebd8200f8e91f31c61125ec9cd9bbdb9db48ce
Marciete Dellarmelina <marciete.dellarmelina@grupoicts.com.br>
2023-11-09 01:22:09 -0400

    teste de depuração

---
+++ aa.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main (void)
+{ 
+int a, b;
+a = 0;
+b = 0;
+
+for(b = 10; b>-2; b--)
+{
+  a= a+1;
+  printf("valor de A = %d \n", a);
+}
+
+  printf("uuuuuuuuu\n");
+
+  printf("uuuuuuuuu\n");
+
+ printf("uuuuuuuuu\n");
+
+  
+  return 0; 
+} 
+
+//gcc aa.c -o aa 
